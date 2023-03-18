d87668fd2331db24d8bf67ae514cad47c3c2f6df
Jakarin-Jojo <jakarin.chujan@gmail.com>
2023-03-18 14:35:42 +0700

    Add adapter

---
+++ app/src/main/java/com/develop/newsapp/ui/fragment/ItemArticlePreview.kt
@@ -0,0 +1,7 @@
+package com.develop.newsapp.ui.fragment
+
+import androidx.fragment.app.Fragment
+import com.develop.newsapp.R
+
+class ItemArticlePreview:Fragment(R.layout.item_article_preview) {
+}
